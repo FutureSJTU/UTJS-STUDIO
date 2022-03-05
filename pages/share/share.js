37a6ba0f6cd96beaa1a8b0997f8848972789bcb2
yusenzheng <yusen_zheng@163.com>
2022-03-05 13:24:29 +0800

    repair tabBar

--- pages/share/share.js
+++ pages/share/share.js
@@ -64,16 +64,3 @@ Page({
 
   }
 })
-
-Component({
-  pageLifetimes: {
-    show() {
-      if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 2
-        })
-      }
-    }
-  }
-})
