f5207ab563d3bf2cfaea4885337fbc46e1be8cdb
yusenzheng <yusen_zheng@163.com>
2022-03-28 20:46:30 +0800

    update

--- pages/study/zhutu/zhutu.js
+++ pages/study/zhutu/zhutu.js
@@ -8,6 +8,30 @@ Page({
 
   },
 
+  showModal(e) {
+    this.setData({
+      modalName: e.currentTarget.dataset.target
+    })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null
+    })
+  },
+  ChooseCheckbox(e) {
+    let items = this.data.checkbox;
+    let values = e.currentTarget.dataset.value;
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      if (items[i].value == values) {
+        items[i].checked = !items[i].checked;
+        break
+      }
+    }
+    this.setData({
+      checkbox: items
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
