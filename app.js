6a6eda431cc3cab8bf3de0dd4f1fbd292be66016
yusenzheng <yusen_zheng@163.com>
2022-03-28 21:03:34 +0800

    merge branch

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // app.js
 App({
+
   onLaunch() {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
@@ -14,6 +15,7 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    total_study_time: 10
   }
 })
