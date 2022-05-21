7a07619267baf0ac6c18cfdb63e00fe3d6164c8b
future <1371780953@qq.com>
2022-05-21 17:21:26 +0800

    提高刷新频率

--- miniprogram/pages/study/baotu/baotu.js
+++ miniprogram/pages/study/baotu/baotu.js
@@ -48,7 +48,7 @@ Page({
         _this.data.timer = setInterval(//定时器  函数赋值给timer  方便clearInterval（）使用
             function () {
                 _this.toClock1();
-            }, 20000);
+            }, 5000);
 
         _this.setData({
             timer: _this.data.timer
